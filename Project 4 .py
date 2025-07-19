0d0cbbb1e4760e7358fbeb111729a96c630659fd
Enerel1203 <enerel.bayanmunkh@ris.ae>
2025-07-19 18:11:26 +0400

    Add files via upload

---
+++ Project 4 .py
@@ -0,0 +1,6 @@
+temperature = float(input("Enter the current temperature in °C: "))
+
+if temperature >= 20:
+    print("It's warm enough! Rohan can wear light clothes.")
+else:
+    print("It's too cold! Rohan should wear something warmer like a jacket or pullover.")
