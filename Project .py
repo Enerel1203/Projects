bf4ba7030f80e8cf169d5302001198b2088320a8
Enerel1203 <enerel.bayanmunkh@ris.ae>
2025-07-19 17:32:39 +0400

    Add files via upload

---
+++ Project .py
@@ -0,0 +1,20 @@
+friend1 = "Ali"
+birthday1 = "January 1"
+
+friend2 = "Thomas"
+birthday2 = "March 15"
+
+friend3 = "Tom"
+birthday3 = "May 21"
+
+friend4 = "Ronald"
+birthday4 = "August 30"
+
+friend5 = "John"
+birthday5 = "December 9"
+
+print(friend1 + "'s birthday is on " + birthday1)
+print(friend2 + "'s birthday is on " + birthday2)
+print(friend3 + "'s birthday is on " + birthday3)
+print(friend4 + "'s birthday is on " + birthday4)
+print(friend5 + "'s birthday is on " + birthday5)
