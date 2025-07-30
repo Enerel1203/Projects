c5dc7b187606f2545c8c5577410ec35d966c53e6
Enerel1203 <enerel.bayanmunkh@ris.ae>
2025-07-30 13:00:58 +0400

    Add files via upload

---
+++ Project 8.py
@@ -0,0 +1,6 @@
+age= int(input("Enter your age: "))
+
+if 10 <= age <= 20:
+      print("You are allowed to enroll in the class.")
+else:
+    print("Sorry, you are not allowed to enroll in the class.")
